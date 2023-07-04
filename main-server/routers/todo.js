d17c45fea6c8346796fccf3b397993419f144aaa
J1min <aoiharu41@gmail.com>
2023-07-04 11:27:02 +0900

    crud

--- main-server/routers/todo.js
+++ main-server/routers/todo.js
@@ -5,25 +5,33 @@ const { getConnection } = require("../models/connecter");
 let database = [];
 
 router.get("/", async (_, res) => {
-  const [results] = await getConnection().execute("SELECT * FROM todo");
+  const [results] = await getConnection().execute(`SELECT * FROM todo`);
   return res.json(results);
 });
 
-router.post("/", (req, res) => {
-  database.push({ ...req.body, id: database.length + 1 });
+router.post("/", async (req, res) => {
+  const { todo, completed } = req.body;
+  await getConnection().execute(`INSERT todo(todo, completed) VALUES(?, ?)`, [
+    todo,
+    completed,
+  ]);
   return res.json("성공");
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   const { id } = req.params;
-  database = database.filter((todo) => todo.id !== Number(id));
+  await getConnection().execute(`DELETE FROM todo WHERE id = ?`, [id]);
   return res.json("성공");
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
   const { id } = req.params;
-  const foundIdx = database.findIndex((todo) => todo.id === Number(id));
-  database[foundIdx] = { ...database[foundIdx], ...req.body };
+  const { todo, completed } = req.body;
+  await getConnection().execute(
+    `UPDATE todo SET todo = ?, completed = ? WHERE id = ?;`,
+    [todo, completed, id]
+  );
+
   return res.json("성공");
 });
 
